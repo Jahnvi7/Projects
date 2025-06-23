69fd0da6230c83c424aa7617b098eca636c5ab96
Jahnvi Suthar <jahnvisuthar7@gmsil.com>
2025-06-23 17:53:56 +0530

    Index Renamed

---
+++ Para.py
@@ -0,0 +1,14 @@
+m = "m"
+f = "f"
+g = input("Whats your gender? (m/f): ")
+
+if g == m:
+    n = input("Whats your Name? : ")
+    print("There was the guy who named " + n + ".")
+    print("He was a good guy. and he is you :)")
+    print("You are handsome, " + n + ".")
+elif g == f:
+    n = input("Whats your Name? : ")
+    print("There was the girl who named " + n + ".")
+    print("She was a good girl. and she is you :)")
+    print("You are beautiful, " + n + ".")
