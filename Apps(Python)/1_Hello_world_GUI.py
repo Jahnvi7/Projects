64657a4a373dc88cfde8a8cef2bae32e8ef24188
Jahnvi Suthar <jahnvisuthar7@gmsil.com>
2025-06-27 17:25:34 +0530

    Renamed variable name

--- Apps(Python)/1_Hello_world_GUI.py
+++ Apps(Python)/1_Hello_world_GUI.py
@@ -1,15 +1,15 @@
 import tkinter as tk
 
 # Creates the main window
-root = tk.Tk()
-root.title("Hello App")
-root.geometry("200x100")  # Sets size of window (width x height)
+w = tk.Tk()
+w.title("Hello App")
+w.geometry("200x100")  # Sets size of window (width x height)
 
 # Creates a label widget
-label = tk.Label(root, text="Hello, World!", font=("Cabili", 16))
+label = tk.Label(w, text="Hello, World!", font=("Cabili", 16))
 
 # Packs the label into the window
 label.pack(pady=20)
                                                                                                                
 # Start the GUI event loop
-root.mainloop()
+w.mainloop()
