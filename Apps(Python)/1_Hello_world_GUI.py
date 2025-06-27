09f7269ca0ea272dc62ffdd0d7aeee0fe7b36f49
Jahnvi Suthar <jahnvisuthar7@gmsil.com>
2025-06-27 17:42:17 +0530

    Renamed variable name

--- Apps(Python)/1_Hello_world_GUI.py
+++ Apps(Python)/1_Hello_world_GUI.py
@@ -6,7 +6,7 @@ w.title("Hello App")
 w.geometry("200x100")  # Sets size of window (width x height)
 
 # Creates a label widget
-label = tk.Label(w, text="Hello, World!", font=("Cabili", 16))
+label = tk.Label(w, text="Hello, World!", font=("Calibri", 16))
 
 # Packs the label into the window
 label.pack(pady=20)
