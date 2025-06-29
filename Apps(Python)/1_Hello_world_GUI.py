99a0a038c8b3c1727a158c6791e7c1b27db99655
Jahnvi Suthar <jahnvisuthar7@gmail.com>
2025-06-29 13:53:52 +0530

    Now used Customtkinter

--- Apps(Python)/1_Hello_world_GUI.py
+++ Apps(Python)/1_Hello_world_GUI.py
@@ -1,15 +1,15 @@
-import tkinter as tk
+import customtkinter as ctk
 
 # Creates the main window
-w = tk.Tk()
+w = ctk.CTk()
 w.title("Hello App")
 w.geometry("200x100")  # Sets size of window (width x height)
 
-# Creates a label widget
-label = tk.Label(w, text="Hello, World!", font=("Calibri", 16))
+# Creates a label widget (Corrected: CTkLabel instead of Label)
+label = ctk.CTkLabel(w, text="Hello, World!", font=("Calibri", 16))
 
 # Packs the label into the window
 label.pack(pady=20)
-                                                                                                               
+
 # Start the GUI event loop
 w.mainloop()
