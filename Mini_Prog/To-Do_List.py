b8fcfb2fcab154abb7ab2936d0da9d29fff39e24
Jahnvi Suthar <jahnvisuthar7@gmail.com>
2025-07-08 17:20:43 +0530

    Added theme

--- Mini_Prog/To-Do_List.py
+++ Mini_Prog/To-Do_List.py
@@ -4,6 +4,8 @@ import customtkinter as ctk
 app = ctk.CTk()
 app.title("To-Do List")
 app.geometry("300x400")
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("blue")
 
 # Entry box for task
 task_entry = ctk.CTkEntry(app, placeholder_text="Enter a task")
@@ -18,8 +20,8 @@ def add_task():
         task_entry.delete(0, 'end')
 
 # Add button
-add_button = ctk.CTkButton(app, text="Add Task", command=add_task)
-add_button.pack(pady=5)
+add_but = ctk.CTkButton(app, text="Add Task", command=add_task)
+add_but.pack(pady=5)
 
 # Run the GUI
 app.mainloop()
